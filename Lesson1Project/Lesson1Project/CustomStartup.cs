fd9f65c402deb219a3d5f3895422dcef1bca2348
Николай Глацких <nglackikh@gmail.com>
2020-12-11 21:39:20 +0200

    Коммит первого ДЗ

---
+++ Lesson1Project/Lesson1Project/CustomStartup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson1Project
+{
+    public class CustomStartup
+    {
+        private IConfiguration _configuration { get; set; }
+
+        public CustomStartup(IConfiguration conf)
+        {
+            _configuration = conf;
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync(_configuration["Logging:LogLevel:Microsoft.Hosting.Lifetime"]);
+                });
+            });
+        }
+
+
+    }
+}

f3c771523415a95dc58dae8838b239a7e021f34f
Николай Глацких <nikolay.glatskikh@metinvestholding.com>
2020-12-17 18:49:50 +0200

    added lesson2 project with another folder architecture

---
+++ Lesson2Project/Lesson2Project/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson2Project.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["indexPhrase"] = "Hello, this is Home/Index method";
+            return View();
+        }
+    }
+}
